661b4470449176f146247bfe091ef56b03843c25
trinity <trinity@macpaw.com>
2022-05-06 15:39:00 +0300

    Adding logic to traffic light project

--- Lesson 2.1/TrafficLight/TrafficLight/ViewController.swift
+++ Lesson 2.1/TrafficLight/TrafficLight/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
+enum TrafficLightState {
+    case off
+    case redLight
+    case yellowLight
+    case greenLight
+}
+
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    // MARK: - Private Properties
+    private var trafficLightState = TrafficLightState.off
+    
+    
+    // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        changeState(to: .off)
     }
-
-
+    
+    // MARK: - IB Actions
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch trafficLightState {
+        case .off:
+            changeState(to: .redLight)
+        case .redLight:
+            changeState(to: .yellowLight)
+        case .yellowLight:
+            changeState(to: .greenLight)
+        case .greenLight:
+            changeState(to: .redLight)
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func setupUI() {
+        redLightView.layer.cornerRadius = 60
+        yellowLightView.layer.cornerRadius = 60
+        greenLightView.layer.cornerRadius = 60
+    }
+    
+    private func changeState(to state: TrafficLightState) {
+        trafficLightState = state
+        redLightView.alpha = state == .redLight ? 1 : 0.5
+        yellowLightView.alpha = state == .yellowLight ? 1 : 0.5
+        greenLightView.alpha = state == .greenLight ? 1 : 0.5
+    }
+    
 }
 
