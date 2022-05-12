b279b3e074d95a9dc6e492d62585f9d9673acf77
trinity <trinity@macpaw.com>
2022-05-12 14:07:55 +0300

    Create new UiKitProject

---
+++ Lesson 2.1/UiKitApp/UiKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiKitApp
+//
+//  Created by trinity on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
