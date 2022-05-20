0be0e7ff4a9b2bc8b5e571fa5323529b1d1910ef
trinity <trinity@macpaw.com>
2022-05-21 00:34:52 +0300

    Adding logic to Forgot User name button

--- Lesson 2.3/LoginView/LoginView/LoginViewController.swift
+++ Lesson 2.3/LoginView/LoginView/LoginViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func forgotUserNameTapped() {
+        let alert = UIAlertController(
+            title: "Oops!",
+            message: "Your name is User \u{1F929}",
+            preferredStyle: .alert
+        )
+        
+        let alertAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(alertAction)
+        
+        present(alert, animated: true)
+    }
+    
 }
-
